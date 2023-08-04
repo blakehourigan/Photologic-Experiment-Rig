aa8b11fe9f054bbe0afb47f1b18d86f5711e1b85
Blake <houriganblake@protonmail.com>
2023-08-04 10:29:07 -0400

    parsing testing data python file

--- Code/laserTestCode/laserTestCodeWorking/laserTestCodeWorking.ino
+++ Code/laserTestCode/laserTestCodeWorking/laserTestCodeWorking.ino
@@ -1,7 +1,9 @@
-const byte inputBit0 = PB4;        // PB4 or pin 10 is the bit that detects a switch to low to increment number of licks       
-      byte inputBit1 = PH5;
+const byte inputBit0 = PH5;        // PB4 or pin 10 is the bit that detects a switch to low to increment number of licks       
+      byte inputBit1 = PH6;
 
 unsigned long startTime, endTime; // variables to store start and endtime of licks
+unsigned long programStart;
+unsigned long currentTime; 
 
 volatile bool pinState0,pinState1;
 volatile bool previousState0 = 1; 
@@ -11,12 +13,6 @@ int licksRight = 0;
 
 void setup() {
   Serial.begin(9600);             // Start the serial communication
-  DDRB |= (1 << DDB5);            // Set pin 7 in the Port B register (pin 13) as output, we want to output the value of this pin to the LED.
-
-
-  DDRH |= (1 << DDH6);            // Set pin 7 in the Port H register (pin 13) as output, we want to output the value of this pin to the LED.
-
-
   // Set up Timer 4 for 1 microsecond interval
   cli();                          // Disable global interrupts
   TCCR4A = 0;                     // Set entire TCCR4A register to 0
@@ -27,13 +23,13 @@ void setup() {
   TCCR4B |= (1 << CS40);          // Set prescaler to 1 (no prescaling)
   TIMSK4 |= (1 << OCIE4A);        // Enable timer compare interrupt
   sei();                          // Enable global
-
+  programStart = millis();
 
 }
 
 ISR(TIMER4_COMPA_vect) {
   // Read inputPin using direct port manipulation
-  pinState0 = (PINB & (1 << inputBit0)) ;    // to know whether or not we have a signal from our photosensor, we AND the value of pinB (0001 0000 if 5V applied), with the value of
+  pinState0 = (PINH & (1 << inputBit0)) ;    // to know whether or not we have a signal from our photosensor, we AND the value of pinB (0001 0000 if 5V applied), with the value of
                                            // 0001 0000. If photosensor sees light, it will send 5v or a 1 to pin 10, giving us a 1 out of the expression, otherwise we get 0. 
   pinState1 = (PINH & (1 << inputBit1)) ; 
 }
@@ -50,7 +46,6 @@ void loop()
   
   if (pinState0 == 0 && previousState0 == 1) {
     startTime = millis();
-
     previousState0 = 0;
   } 
   if (pinState0 == 1 && previousState0 == 0) {
@@ -62,7 +57,9 @@ void loop()
     Serial.print(myString + licksLeft + "\n");
     Serial.print("lick time:");
     Serial.println(endTime - startTime);
-    Serial.println();
+    currentTime= endTime - programStart;
+    Serial.println(currentTime / 1000.0 );
+    Serial.println("\n");
    
 
     previousState0 = 1;
@@ -83,6 +80,8 @@ void loop()
     Serial.print(myString + licksRight + "\n");
     Serial.print("lick time:");
     Serial.println(endTime - startTime);
+    currentTime= endTime - programStart;
+    Serial.println(currentTime / 1000.0 );
     Serial.println("\n");
     previousState1 = 1;
 
