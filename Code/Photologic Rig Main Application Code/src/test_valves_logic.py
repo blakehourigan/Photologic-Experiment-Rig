a2c6a04543125f6788a55154cb9d37d7252a379a
Blake <houriganblake@protonmail.com>
2024-03-15 14:11:39 -0400

    print line

--- Code/Photologic Rig Main Application Code/src/test_valves_logic.py
+++ Code/Photologic Rig Main Application Code/src/test_valves_logic.py
@@ -29,6 +29,7 @@ class valveTestLogic:
         while True:
             if self.controller.arduino_mgr.motor_arduino.in_waiting:
                 line = self.controller.arduino_mgr.motor_arduino.readline().decode('utf-8').rstrip()
+                print(line)
                 if line == "Finished loop":
                         if valve != 0:
                             side_one_volumes.append(self.controller.valve_testing_window.input_popup(valve))
