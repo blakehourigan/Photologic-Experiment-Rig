e0669f561b257d4dbd6b82078d1db597b4ff3ab2
Blake <48300180+blakehourigan@users.noreply.github.com>
2024-05-08 09:06:59 -0400

    Update motor_code_final.ino

--- Code/PlatformIO/Projects/motor_final/src/motor_code_final.ino
+++ Code/PlatformIO/Projects/motor_final/src/motor_code_final.ino
@@ -338,7 +338,7 @@ void create_timestamp(String command)
 void handle_motor_command(String command)
 {
   const int STEPPER_UP_POSITION = 0;
-  const int STEPPER_DOWN_POSITION = 6100;
+  const int STEPPER_DOWN_POSITION = 6000;
 
   create_timestamp(command);
 
