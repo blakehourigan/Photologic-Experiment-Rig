8d6d1d73d409aa62bfc97608a6c02e0fedd80f05
blakehourigan <houriganblake@protonmail.com>
2025-03-17 13:48:46 -0400

    resolve gh issue #17, band-aid solution to issue #26

--- Code/ArduinoCode/ArduinoCode.ino
+++ Code/ArduinoCode/ArduinoCode.ino
@@ -96,19 +96,105 @@ void loop() {
   
   // a pointer to the side_data for the side that was licked most recently
   static SideData * side_data;
- 
+  
+  if (Serial.available() > 0) {
+    // read until the newline char 
+    command = Serial.readStringUntil('\n');
+  }
+  // only process command cases if one has been recieved from the controller
+  if (!command.equals("\0")){
+    if (command.equals("BEGIN OPEN VALVES")) {
+      // begin opening valves from this point forward, 
+      // this is called when sample time begins
+      open_valves = true;
+    }
+    else if (command.equals("STOP OPEN VALVES")) {
+      // stop opening valves
+      open_valves = false;
+    }
+    else if (command.equals("TRIAL START")){
+      trial_start_time = millis(); 
+    }
+    else if (command.equals("T=0")){
+      // mark t=0 time for the arduino side
+      program_start_time = millis();
+
+      trial_start_time = program_start_time;
+    }
+    else if (command.equals("UP")) {
+      // tell door motor to move up
+      noInterrupts();
+      stepper.moveTo(STEPPER_UP_POSITION);
+      interrupts();
+
+      motor_time.movement_start = millis();
+      motor_time.movement_type = "UP"; 
+
+      previous_command = command;
+      motor_running = true;
+    }
+    else if (command.equals("DOWN")){
+      // tell door motor to move down
+      stepper.moveTo(STEPPER_DOWN_POSITION);
+
+      motor_time.movement_start = millis();
+      motor_time.movement_type = "DOWN"; 
+
+      previous_command = command;
+      motor_running = true;
+    }
+    else if (command.equals("RESET")){
+      // reset the board
+      wdt_enable(WDTO_1S);
+    }
+    else if(command.equals("PRIME VALVES")){
+      prime_valves();             
+    }
+    else if (command.equals("TEST VOL")){
+      run_valve_test(side_one_data.valve_durations, side_two_data.valve_durations);
+    }
+    else if (command.equals("OPEN SPECIFIC")){
+      control_specific_valves(); 
+    }
+    else if (command.equals("REC DURATIONS")){
+      // receive valve durations from the python controller
+      durations = receive_durations();
+    }
+    else if (command.equals("REC SCHED")){
+      // handle recieving program schedule
+      schedules = receive_schedules();
+    }
+    else if (command.equals("REC VAR")){
+      // handle recieving program variables (num_stim, num_trials)
+      receive_exp_variables();
+    }
+    else if (command.equals("VER SCHED")){
+      schedule_verification(schedules);
+    }
+    else if (command.equals("VER DURATIONS")){
+      durations_verification(durations);
+    }
+    else {
+      Serial.print("Unknown command received: ");
+      if (command == "\0"){
+        Serial.println("null command");
+      }
+      Serial.println(command);
+    }
+  }
+  
   // check if motor_running first to avoid unneccessary calls to stepper.distanceToGo()
   if (motor_running && stepper.distanceToGo() == 0) {
     // at this stage we have filled movement_start, movement_type, and now movement_end
     motor_time.movement_end = millis();
     report_motor_movement(previous_command, motor_time, program_start_time, trial_start_time);
-    
+
     if(previous_command.equals("UP")){
       current_trial++;
     }
     motor_running = false;
   }
-  
+
   // read current optical sensor pin values
   side_one_pin_state = (PINL & (1 << OPTICAL_DETECTOR_BIT_SIDE1));
   side_two_pin_state = (PINL & (1 << OPTICAL_DETECTOR_BIT_SIDE2));
@@ -124,27 +210,27 @@ void loop() {
 
     if (!handling_lick){
       // if no previous lick or valve movement is being handled
-        bool lick_start_one = lick_started(side_one_data);
-        bool lick_start_two = lick_started(side_two_data);
+      bool lick_start_one = lick_started(side_one_data);
+      bool lick_start_two = lick_started(side_two_data);
 
-        if (lick_start_one || lick_start_two){
-          if(lick_start_one){
-            side_data = &side_one_data;
-            lick_start_one= false;
-          }else{
-            side_data = &side_two_data;
-            lick_start_two = false;
-          }
-          // record lick start time 
-          lick_time.lick_begin_time = millis();
-          handling_lick = true;
+      if (lick_start_one || lick_start_two){
+        if(lick_start_one){
+          side_data = &side_one_data;
+          lick_start_one= false;
+        }else{
+          side_data = &side_two_data;
+          lick_start_two = false;
+        }
+        // record lick start time 
+        lick_time.lick_begin_time = millis();
+        handling_lick = true;
 
-          if (open_valves){
-            open_valve(side_data, current_trial);
-            valve_time.valve_open_time = micros();
-          }
+        if (open_valves){
+          open_valve(side_data, current_trial);
+          valve_time.valve_open_time = micros();
         }
       }
+    }
     else if(handling_lick){
       // check for lick end / valve end and record data
       if (!open_valves) {
@@ -155,7 +241,7 @@ void loop() {
         close_all();
         lick_marked = false;
         valve_marked = false;
-        
+
         if (lick_ended(side_data)){ 
           lick_time.lick_end_time= millis();
           // in the case that we WERE in open valves, but moved out of a sample time without
@@ -195,91 +281,7 @@ void loop() {
   // update the side leds that flash when a lick is detected on a side
   update_leds(side_one_pin_state, side_two_pin_state);
 
-  if (Serial.available() > 0) {
-    // read until the newline char 
-    command = Serial.readStringUntil('\n');
-  }
-  // only process command cases if one has been recieved from the controller
-  if (!command.equals("\0")){
-    if (command.equals("BEGIN OPEN VALVES")) {
-      // begin opening valves from this point forward, 
-      // this is called when sample time begins
-      open_valves = true;
-    }
-    else if (command.equals("STOP OPEN VALVES")) {
-      // stop opening valves
-      open_valves = false;
-    }
-    else if (command.equals("TRIAL START")){
-      trial_start_time = millis(); 
-    }
-    else if (command.equals("T=0")){
-      // mark t=0 time for the arduino side
-      program_start_time = millis();
 
-      trial_start_time = program_start_time;
-    }
-    else if (command.equals("UP")) {
-      // tell door motor to move up
-      noInterrupts();
-      stepper.moveTo(STEPPER_UP_POSITION);
-      interrupts();
-      
-      motor_time.movement_start = millis();
-      motor_time.movement_type = "UP"; 
-
-      previous_command = command;
-      motor_running = true;
-    }
-    else if (command.equals("DOWN")){
-      // tell door motor to move down
-      stepper.moveTo(STEPPER_DOWN_POSITION);
-      
-      motor_time.movement_start = millis();
-      motor_time.movement_type = "DOWN"; 
-
-      previous_command = command;
-      motor_running = true;
-    }
-    else if (command.equals("RESET")){
-      // reset the board
-      wdt_enable(WDTO_1S);
-    }
-    else if(command.equals("PRIME VALVES")){
-      prime_valves();             
-        }
-    else if (command.equals("TEST VOL")){
-      run_valve_test(side_one_data.valve_durations, side_two_data.valve_durations);
-    }
-    else if (command.equals("OPEN SPECIFIC")){
-      control_specific_valves(); 
-    }
-    else if (command.equals("REC DURATIONS")){
-      // receive valve durations from the python controller
-      durations = receive_durations();
-    }
-    else if (command.equals("REC SCHED")){
-      // handle recieving program schedule
-      schedules = receive_schedules();
-    }
-    else if (command.equals("REC VAR")){
-      // handle recieving program variables (num_stim, num_trials)
-      receive_exp_variables();
-    }
-    else if (command.equals("VER SCHED")){
-      schedule_verification(schedules);
-    }
-    else if (command.equals("VER DURATIONS")){
-      durations_verification(durations);
-    }
-    else {
-      Serial.print("Unknown command received: ");
-      if (command == "\0"){
-        Serial.println("null command");
-      }
-      Serial.println(command);
-    }
-  }
   stepper.run();
 }
 
